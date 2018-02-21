92191ffdc4f3ab296e9d5d4776d56bf8b27283b5
victorloo <ila9970@gmail.com>
2018-02-21 15:54:06 +0800

    add hero attack event

--- js/index.js
+++ js/index.js
@@ -86,3 +86,54 @@ class Monster extends BaseCharacter {
 //測試是否成功創建物件
 var hero = new Hero("Bernard", 130, 30);
 var monster = new Monster("Skeleton", 130, 10);
+
+//開始戰鬥流程
+function addSkillEvent() {
+  var skill = document.getElementById("skill");
+  skill.onclick = function() {
+    heroAttack();
+  }
+}
+addSkillEvent();
+
+//回合結束的機制
+var rounds = 10;
+function endTurn() {
+  rounds--;
+  document.getElementById("round-num").textContent = rounds;
+  if (rounds < 1) {
+    //遊戲結束
+  }
+}
+
+//英雄和怪獸動作的時間軸
+function heroAttack() {
+  document.getElementsByClassName("skill-block")[0].style.display = "none";
+  //英雄攻擊
+  setTimeout(function() {
+    hero.element.classList.add("attacking");
+    setTimeout(function() {
+      hero.attack(monster);
+      hero.element.classList.remove("attacking");
+    }, 500)
+  }, 100);
+  //怪獸攻擊
+  setTimeout(function() {
+    if (monster.alive) {
+      monster.element.classList.add("attacking");
+      setTimeout(function() {
+        monster.attack(hero);
+        monster.element.classList.remove("attacking");
+        endTurn();
+        //判斷英雄是否死亡
+        if (hero.alive == false) {
+          //遊戲結束
+        } else {
+          document.getElementsByClassName("skill-block")[0].style.display = "block";
+        }
+      }, 500);
+    } else {
+      //如果怪獸已經死亡，結束遊戲
+    }
+  }, 1100);
+}
